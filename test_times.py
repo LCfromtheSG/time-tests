f351c5fd33b15480793c66e26698690d183e5c8f
Wei Hong <weihong.chua19@sps.nus.edu.sg>
2024-10-28 10:34:42 +0000

    Added_tests stated in issue 12

--- test_times.py
+++ test_times.py
@@ -8,3 +8,24 @@ def test_given_input():
         expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
         assert result == expected
 
+def test_no_overlap():
+    if __name__ == "__main__":
+        large = time_range("2010-01-12 10:00:00", "2010-01-12 10:15:00")
+        short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+        result1 = compute_overlap_time(large,short)
+        assert all(x > y for x,y in result1)
+
+def test_sev_intervals():
+    if __name__ == "__main__":
+        large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00",2,60)
+        short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+        result = compute_overlap_time(large,short)
+        expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+        assert result == expected
+
+def test_end_equals_start():
+    if __name__ == "__main__":
+        large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00",2,60)
+        short = time_range("2010-01-12 12:00:00", "2010-01-12 12:45:00", 2, 60)
+        result = compute_overlap_time(large,short)
+        assert all(x > y for x,y in result)
